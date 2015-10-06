70754d1d8dacaaea6c76b229670604a90010fd74
talum <tracidini@gmail.com>
2015-10-06 16:47:27 -0400

    Refactored to use interface instead of @

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,5 @@
 # your code goes here
+#best practice is to use the method if you have it. 
 class Person
   attr_accessor :bank_account
   attr_reader :name, :happiness, :hygiene
@@ -42,34 +43,34 @@ class Person
   end
 
   def get_paid(salary)
-    @bank_account += salary 
+    self.bank_account += salary 
     "all about the benjamins"
   end
 
   def take_bath
-    self.hygiene = @hygiene + 4
+    self.hygiene = hygiene + 4 #don't need self because it is implied, but it helped the test pass
     "♪ Rub-a-dub just relaxing in the tub ♫"
   end
   
   def work_out
-    self.happiness = @happiness + 2
-    self.hygiene = @hygiene - 3
+    self.happiness = self.happiness + 2
+    self.hygiene = self.hygiene - 3
     "♪ another one bites the dust ♫"
   end
 
   def call_friend(friend)
-    self.happiness = @happiness + 3
+    self.happiness = self.happiness + 3
     friend.happiness += 3
     "Hi #{friend.name}! It's #{self.name}. How are you?"
   end
 
   def start_conversation(person, topic)
     if topic == "politics"
-      self.happiness = @happiness - 2
+      self.happiness = self.happiness - 2
       person.happiness-= 2
       "blah blah partisan blah lobbyist"
     elsif topic == "weather"
-      self.happiness = @happiness + 1
+      self.happiness = self.happiness + 1
       person.happiness += 1
       "blah blah sun blah rain"
     else
